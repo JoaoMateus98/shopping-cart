4e13f73bd2201c71f842d3295a5f889407486c0b
JoaoMateus98 <joaobsu98@gmail.com>
2023-01-12 12:42:08 -0500

    fetch only at shop page not at start of app

--- src/App.js
+++ src/App.js
@@ -5,28 +5,8 @@ import Home from "./components/home/Home";
 import Shop from "./components/shop/Shop";
 import Cart from "./components/cart/Cart";
 
-async function fetchGames(numOfGames) {
-  try {
-    const response = await fetch(
-      `https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&page_size=${numOfGames}`
-    );
-    const responseJson = await response.json();
-
-    return responseJson.results;
-  } catch {
-    console.log("failed to retrive games");
-  }
-}
-
 const App = () => {
   const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    if (games.length === 0) {
-      fetchGames(10).then((val) => setGames(games.concat(val)));
-      console.log("fetched");
-    }
-  }, [games]);
 
   return (
     <Router>
@@ -36,7 +16,10 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop gameList={games} />} />
+          <Route
+            path="/shop"
+            element={<Shop gameList={games} setGames={setGames} />}
+          />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </main>
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,11 +1,27 @@
 import { useState, useEffect } from "react";
 
-const Shop = ({ gameList }) => {
+async function fetchGames(numOfGames) {
+  try {
+    const response = await fetch(
+      `https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&page_size=${numOfGames}`
+    );
+    const responseJson = await response.json();
+
+    return responseJson.results;
+  } catch {
+    console.log("failed to retrive games");
+  }
+}
+
+const Shop = ({ gameList, setGames }) => {
   const [gameImage, setGameImage] = useState(null);
 
   useEffect(() => {
     if (gameList.length !== 0) {
       setGameImage(testCard(gameList));
+    } else {
+      fetchGames(20).then((val) => setGames(gameList.concat(val)));
+      console.log("fetched");
     }
   }, [gameList]);
 
