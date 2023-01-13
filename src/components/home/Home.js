5889434aa890316749445fd86b1a105964a6d08d
JoaoMateus98 <joaobsu98@gmail.com>
2023-01-12 22:08:48 -0500

    finished styling shop and home screens

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,18 @@
 import "./Home.css";
+import homeImage from "../../images/home.png";
 
-function App() {
-  return <div>Home Page</div>;
+function Home() {
+  return (
+    <div className="home-container">
+      <section className="message-container">
+        <h1>Welcome To The World Of Gaming</h1>
+        <p>THE WORLDS FIRST GAMING MARKETPLACE</p>
+      </section>
+      <section className="home-image-container">
+        <img src={homeImage} alt="" />
+      </section>
+    </div>
+  );
 }
 
-export default App;
+export default Home;
