1de4ae39224ed5726abce183110abdc34c7da893
JoaoMateus98 <joaobsu98@gmail.com>
2023-01-12 20:58:30 -0500

    background and navbar styled

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Nav.css";
+import cartImage from "../images/shopping-cart32.png";
 
 const Nav = () => {
   return (
@@ -12,7 +13,9 @@ const Nav = () => {
           <li>MarketPlace</li>
         </Link>
         <Link to="/cart">
-          <li>Cart</li>
+          <li>
+            <img src={cartImage} alt="" />
+          </li>
         </Link>
       </ul>
     </nav>
