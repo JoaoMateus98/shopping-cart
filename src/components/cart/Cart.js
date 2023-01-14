c96d7715a8f083ee4403fe15cdea1f688e94af02
JoaoMateus98 <joaobsu98@gmail.com>
2023-01-14 13:42:44 -0500

    started working on CartItem and CartWithItems components

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,5 +1,32 @@
+import { useContext } from "react";
+import { CartContext } from "../../helpers/CartProvider";
+import { countTotalQuantity } from "../../helpers/handleCartItems";
+import "./Cart.css";
+
 const Cart = () => {
-  return <div>Cart Page</div>;
+  const { cartItems, setCartItems } = useContext(CartContext);
+  return <CartWithItems cartItems={cartItems} />;
 };
+
+const CartWithItems = ({ cartItems }) => {
+  return (
+    <>
+      <section className="cart-section">
+        <section className="cart-header">
+          <h1>{`Your Cart (${countTotalQuantity(cartItems)} items)`}</h1>
+        </section>
+        <section className="cart-items"></section>
+      </section>
+      <section className="checkout-section">
+        <section></section>
+        <section></section>
+      </section>
+    </>
+  );
+};
+
+const CartItem = () => {};
+
+// TODO: createCartWithNoItems
 
 export default Cart;
--- src/components/shop/Shop.test.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Shop, { fetchGames } from "./Shop";
-import { testGameObject } from "../../helpers/testGameObject";
-
-test.todo("write test");
