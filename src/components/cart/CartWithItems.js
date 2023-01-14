7fdd51345c9df194f0ce3a9536c956e2feb0aa3f
JoaoMateus98 <joaobsu98@gmail.com>
2023-01-14 15:22:10 -0500

    checkout section css complete for now

--- src/components/cart/CartWithItems.js
+++ src/components/cart/CartWithItems.js
@@ -4,14 +4,40 @@ const CartWithItems = ({ cartItems }) => {
   return (
     <>
       <section className="cart-section">
-        <section className="cart-header">
-          <h1>{`Your Cart (${countTotalQuantity(cartItems)} items)`}</h1>
-        </section>
-        <section className="cart-items"></section>
+        <div className="cart-header">
+          <h1 className="sticky-section">{`Your Cart (${countTotalQuantity(
+            cartItems
+          )} items)`}</h1>
+        </div>
+        <div className="cart-items">
+          <div className="item-left">
+            <p className="sticky-section">Item</p>
+          </div>
+          <div className="item-right">
+            <div className="sticky-section">
+              <p>Price</p>
+              <p>Quantity</p>
+              <p>Total</p>
+            </div>
+          </div>
+        </div>
       </section>
       <section className="checkout-section">
-        <section></section>
-        <section></section>
+        <div className="price-calculation-container">
+          <div>
+            <p className="bold-section">Subtotal:</p>
+            <p>$900.57</p>
+          </div>
+          <div>
+            <p className="bold-section">Sales Tax:</p>
+            <p>$100.00</p>
+          </div>
+          <div id="grand-total-container">
+            <p className="bold-section">Grand Total:</p>
+            <p className="grand-total">$1,000.57</p>
+          </div>
+        </div>
+        <button className="checkout-button">Check Out</button>
       </section>
     </>
   );
